b63263aaff21f3a4137f51d47ede92f75322fd1c
emmanuel cabassot <emmanuel.cabassot@laplateforme.io>
2021-03-08 10:24:23 +0100

    Jour05 fini

---
+++ Job05/script.js
@@ -0,0 +1,9 @@
+function afficherJourSemaine(){
+    var joursSemaine = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+    for (let index = 0; index < joursSemaine.length; index++) {
+        console.log(joursSemaine[index]);
+        
+    }
+}
+
+afficherJourSemaine();
