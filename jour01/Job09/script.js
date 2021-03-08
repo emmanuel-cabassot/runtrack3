01430c27a8ad70cd1f83701ab70c3dab129269ec
emmanuel cabassot <emmanuel.cabassot@laplateforme.io>
2021-03-08 22:29:12 +0100

    Petite reflexion du soir pour finir le job09

--- jour01/Job09/script.js
+++ jour01/Job09/script.js
@@ -1,10 +1,50 @@
-var tableau = [0, 5, 7, 12, 5874]
-let grand = 0
-function tri(tableau){
+var tableau = [0, 7, 5, 12, 0, 1]
+
+
+/* function order() {
     tableau.forEach(element => {
-        console.log(element)
-        
+        order();
     });
+} */
+
+function tri(tableau, desc) {
+
+    if (desc == 'asc') {
+
+        var modifie;
+        do {
+            modifie = false;
+            for (var index = 0; index < tableau.length - 1; index++) {
+                if (tableau[index] > tableau[index + 1]) {
+                    var tempo = tableau[index];
+                    tableau[index] = tableau[index + 1];
+                    tableau[index + 1] = tempo;
+                    modifie = true;
+                }
+            }
+        } while (modifie == true);
+
+        console.log(tableau);
+    }
+    else if (desc == 'desc') {
+        var modifie;
+        do {
+            modifie = false;
+            for (var index = 0; index < tableau.length - 1; index++) {
+                if (tableau[index] < tableau[index + 1]) {
+                    var tempo = tableau[index];
+                    tableau[index] = tableau[index + 1];
+                    tableau[index + 1] = tempo;
+                    modifie = true;
+                }
+            }
+        } while (modifie == true);
+
+        console.log(tableau);
+    }
+    else{
+        console.log('dans la fonction tri le deuxieme element doit etre \'desc\' ou \'asc\'')
+    }
 }
 
-tri(tableau)
+tri(tableau, 'asc')
