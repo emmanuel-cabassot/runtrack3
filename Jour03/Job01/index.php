4aa341e23c04391d3724b2f746710e19ea13c2fe
emmanuel cabassot <emmanuel.cabassot@laplateforme.io>
2021-03-10 09:55:05 +0100

    Jour3 Job01 fini

--- Jour03/Job01/index.php
+++ Jour03/Job01/index.php
@@ -1,9 +1,3 @@
-Job 01
-Créez une balise button ayant comme id “button”. En cliquant sur le
-bouton, vous devrez faire apparaître le texte “le $ est un bon élément
-monétaire. Le # de twitter est une bonne I D. Il faudra faire le point avec la
-classe pour cacher cet élément. ”.
-En dessous faire un bouton qui servira à cacher tout l’élément html.
 <!DOCTYPE html>
 <html lang="fr">
 
--- Jour03/Job01/script.js
+++ Jour03/Job01/script.js
@@ -1,32 +1,17 @@
-/* let article = document.querySelector('article');
-let button  = document.querySelector('#button');
-let cacher  = document.querySelector('#cacher')
 
- */
-let hidden = false;
-let article = $('article').css('color', 'red');
-let button = $('#button').css('background-color', 'blue');
-let cacher = $('#cacher').css('background-color', 'red');
+let article = $('article').css('display', 'none');
+let button = $('#button');
+let cacher = $('#cacher').css('display', 'none');
 
-if (hidden == true) {
-    $('article').css('display', 'none');
-    $('#cacher').css('display', 'none');
-   $('#button').on('click', function () {
+$('#button').on('click', function () {
         $('#cacher').css('display', 'block');
         $('#button').css('display', 'none');
-        $('article').css('display', 'block');
-        hidden = 'bb'
+        $('article').css('display', 'block');     
     });
 
-}
-
-
    $('#cacher').on('click', function () {
-       console.log(hidden)
         $('#cacher').css('display', 'none');
         $('#button').css('display', 'block');
         $('article').css('display', 'none');
-        
-        hidden = true;
     });
 
